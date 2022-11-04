c52c9651c9c2cca0a5f4fbd6226827c9481e70c2
mintradmd <mintra.domundee@sap.com>
2022-11-04 09:13:47 +0100

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header  from './components/header/Header'
+import Navbar from './components/navbar/Navbar'
+import About from './components/about/About'
+
+const App = () => {
+  console.log('hellloooo')
+  return (
+    <div >
+      
+      <Header />
+      <Navbar />
+      <About />
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+const Header = () => {
+  return (
+      <div className='container header_container'>
+        <h5>Helloooooooooooo</h5>
+        <h1>Mintra</h1>
+        hi
+      </div>
+    )
+  }
+
+export default Header
